c4b4b75e31d73943e924ec0022425ea7482d5292
vintry <oennabati3@gmail.com>
2025-04-01 17:09:41 +0200

    commit14

--- BuildWeek5-BE/models/Farmacia/UsoProdotto.cs
+++ BuildWeek5-BE/models/Farmacia/UsoProdotto.cs
@@ -1,6 +1,21 @@
-﻿namespace BuildWeek5_BE.models.Farmacia
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BuildWeek5_BE.Models.Farmacia
 {
     public class UsoProdotto
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Descrizione { get; set; }
+
+        [Required]
+        public int ProdottoId { get; set; }
+
+        [ForeignKey("ProdottoId")]
+        public Prodotto Prodotto { get; set; }
     }
 }
