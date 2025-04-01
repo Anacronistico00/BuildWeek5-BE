e68c598df379d2a621289cf75ada92437b48a38e
vintry <oennabati3@gmail.com>
2025-04-01 10:47:45 +0200

    better ricoveroDTOs error

--- BuildWeek5-BE/DTOs/Ricoverodto/CreateRicoveroDto.cs
+++ BuildWeek5-BE/DTOs/Ricoverodto/CreateRicoveroDto.cs
@@ -5,16 +5,16 @@ namespace BuildWeek5_BE.DTOs
 {
     public class CreateRicoveroDto
     {
-        [Required]
+        [Required(ErrorMessage = "L'ID del paziente è obbligatorio")]
         public int PuppyId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "La data di inizio ricovero è obbligatoria")]
         public DateOnly DataInizioRicovero { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "La descrizione del ricovero è obbligatoria")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "La descrizione deve essere compresa tra {2} e {1} caratteri")]
         public string Descrizione { get; set; }
 
         public DateOnly? DataFineRicovero { get; set; }
-
     }
 }
