c6858175dba73675f634ee137fd919d79548fcf5
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-04-01 12:53:17 +0200

    Added Farmacia Dtos

---
+++ BuildWeek5-BE/DTOs/Farmacia/CassettoDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuildWeek5_BE.DTOs.Farmacia
+{
+    public class CassettoDto
+    {
+        public int CassettoId { get; set; }
+
+        [Required]
+        public int ArmadiettoId { get; set; }
+    }
+}
---
+++ BuildWeek5-BE/DTOs/Farmacia/UsoProdottoDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuildWeek5_BE.DTOs.Farmacia
+{
+    public class UsoProdottoDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Descrizione { get; set; }
+    }
+}
