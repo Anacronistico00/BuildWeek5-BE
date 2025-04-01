dcff9d75db83e26c44a111567d670c278fe1c278
vintry <oennabati3@gmail.com>
2025-04-01 17:06:13 +0200

    commit13

--- BuildWeek5-BE/DTOs/Farmacia/UsoProdottoDto.cs
+++ BuildWeek5-BE/DTOs/Farmacia/UsoProdottoDto.cs
@@ -1,6 +1,11 @@
-﻿namespace BuildWeek5_BE.DTOs.Farmacia
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuildWeek5_BE.DTOs.Farmacia
 {
     public class UsoProdottoDto
     {
+        [Required]
+        [StringLength(100)]
+        public string Descrizione { get; set; }
     }
 }
